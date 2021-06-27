f113ad00cfadb06ebfbd3d4aef4c97743bfa3654
Abhinav <abhinav.zimmy@gmail.com>
2021-06-27 15:17:51 +0530

    added cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import AppNavbar from './components/navbar'
-import Animes from './components/animes'
+import AppNavbar from "./components/navbar";
+import Animes from "./components/animes";
+import CardContainer from "./components/cardContainer";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 
-
 class App extends Component {
-  render() { 
+  render() {
     return (
-      <Router>
-        <div className="App">
-          <AppNavbar />
-          <ToastContainer />
-          <main className="container h-100">
-            <Switch>
-              <Route exact path="/" component={Animes} />
-              {/* <Redirect to="/not-found"/> */}
-            </Switch>
-          </main>
-        </div>
-      </Router>
-     );
+      <div className="App">
+        <AppNavbar />
+
+        <ToastContainer />
+        <main className="container h-100">
+          <Switch>
+            <CardContainer />
+            <Route exact path="/" component={Animes} />
+            {/* <Redirect to="/not-found"/> */}
+          </Switch>
+        </main>
+      </div>
+    );
   }
 }
- 
+
 export default App;
