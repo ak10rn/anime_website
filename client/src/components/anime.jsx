4eb7a625f9429d70846868359766bc984928319f
Abhi <akk5073028@gmail.com>
2021-07-02 23:34:03 +0530

    anime page modified

--- client/src/components/anime.jsx
+++ client/src/components/anime.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { getAnimeByMalId } from "../services/animeService";
 import "./anime.css";
 import ReactReadMoreReadLess from "react-read-more-read-less";
 
@@ -25,14 +24,14 @@ class Anime extends Component {
     reviews: [
       {
         name: "anonymous",
-        img: "http://placekitten.com/30/30",
+        img: "http://placekitten.com/300/340",
         comment: `Oh dear Shingeki no Kyojin, where do I even begin. If you've talked with your friends about anime, then the couple anime that everyone talks about are Naruto, Bleach, One Piece, Dragon Ball, and... Shingeki no Kyojin. What's the difference between Shingeki and the rest? Shingeki only has 25 episodes so far yet it's on par in popularity with the other super long, Americanized anime. Why is it popular? Well that's simply because it's stunningly amazing. Those people that call Shingeki no Kyojin "overrated" may not have the same taste as me, and that's perfectly fine, but in my honest opinion, Shingeki no Kyojin is one of if not the greatest anime to be made. It's not popular for no reason.`,
         userRating: "6",
         id: 132434,
       },
       {
         name: "mikasa ackerman",
-        img: "http://placekitten.com/200/300",
+        img: "http://placekitten.com/230/300",
         comment: `The story is one of the most captivating stories I've ever seen. 100 years prior to the start of the anime, humanity has been on the bridge of extinction due to the monstrous humanoid Titans that devour humans. Now, present day in the anime, the remaining small population of mankind lives confined within 3 "heavenly" walls that are so tall and sturdy that even the titans can't break in. The most outward wall was named, Wall Maria, the middle wall was named Wall Rose, and the most outward wall named Wall Sina. Unfortunately for mankind, a colossal titan, one that is even bigger than the 50 meter heavenly walls, breaks Wall Maria, allowing the other titans to rampage the city, thus leading to another massacre of mankind. During this massacre, our main characters, Eren Yeager and Mikasa Ackerman watch in horror as a horrifying titan rips their mother's head off, then gobbles her up whole. Vowing that he'd one day avenge mankind and exterminate all the titans, Eren Yeager trains to become a survey corp, brave heroic soldiers who go outside the walls, into the plains in order to fight the titans. But we soon find out, that Eren is much more special than he seems, not only is he a brave warrior, but he's also something else that could be the key to humanity's survival, but could also be humanity's destruction.`,
         userRating: "8",
         id: 4334,
@@ -49,8 +48,9 @@ class Anime extends Component {
   handleAddReview = () => {
     // open modal
 
-    // receive response from modal
-    let newReview = {
+    // receive new review and db ka lawda lassan
+
+    const newReview = {
       name: "eren",
       img: "http://placekitten.com/200/300",
       comment: "new comment",
@@ -64,18 +64,18 @@ class Anime extends Component {
   render() {
     return (
       <>
-        <div className="container text-light">
+        <div className="container text-light d-flex flex-column">
           <div className="title">
             <p>{this.state.anime.title}</p>
           </div>
-          <div className="anime-box">
-            <div className="left-panel">
-              <div className="anime-img">
-                <img src={this.state.anime.img} alt="img" />
-              </div>
+
+          <div className="anime-details d-flex flex-row">
+            <div className="anime-img">
+              <img src={this.state.anime.img} alt="img" />
             </div>
-            <div className="anime-body">
-              <div className="anime-det">
+
+            <div className="anime-body d-flex flex-column">
+              <div className="d-flex flex-row ms-3">
                 <div className="anime-rating">
                   <h5>Rating</h5>
                   <i
@@ -85,70 +85,68 @@ class Anime extends Component {
                   <span> {this.state.anime.rating}/10</span>
                 </div>
               </div>
-              <div className="anime-block mt-3">
+              <div className="anime-block mt-3 ms-3">
                 <span>Episodes: {this.state.anime.episodes}</span>
               </div>
-              <div className="anime-block">
+              <div className="anime-block ms-3">
                 <span>Genre: {this.state.anime.genre.join(", ")}</span>
               </div>
-              <div className="anime-sypnosis">
+              <div className="anime-sypnosis ms-3 ">
                 <h5 className="underlineUnder">Sypnosis</h5>
                 <p>{this.state.anime.sypnosis}</p>
               </div>
-
-              {/* Review */}
-
-              <div className="anime-reviews">
-                <div className="anime-reviews-head mb-4 position-relative">
-                  <h5 className="underlineUnder">Reviews</h5>
-                  <button
-                    className="btn btn-primary position-absolute"
-                    style={{ right: 0 }}
-                    onClick={this.handleAddReview}
-                  >
-                    <i className="fa fa-plus" /> Add Review
-                  </button>
-                </div>
-                {this.state.reviews.map((review) => {
-                  return (
-                    <div
-                      key={review.id + "anime-review"}
-                      className="anime-review"
-                    >
-                      <div key={review.id + "user-info"} className="user-info">
-                        <img
-                          key={review.id + "img"}
-                          src={review.img}
-                          alt="User Image"
-                        />
-                        <div key={review.id + "div"}>
-                          <strong key={review.id + "strong"}>
-                            {" "}
-                            {review.name}
-                          </strong>
-                          <span key={review.id + "span"}>
-                            Rated {review.userRating} out of 10
-                          </span>
-                        </div>
-                      </div>
-                      <div
-                        key={review.id + "user-comment"}
-                        className="user-comment"
-                      >
-                        {/* <p >{review.comment}</p> */}
-                        <ReactReadMoreReadLess
-                          charLimit={400}
-                          readMoreText={"Read more ▼"}
-                          readLessText={"Read less ▲"}
-                        >
-                          {review.comment}
-                        </ReactReadMoreReadLess>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
             </div>
+          </div>
+
+          <div className="anime-reviews d-flex flex-column">
+            <div className="d-flex flex-row mb-4 position-relative">
+              <h5 className="underlineUnder">Reviews</h5>
+              <button
+                className="btn btn-primary position-absolute"
+                style={{ right: 0 }}
+                onClick={this.handleAddReview}
+              >
+                <i className="fa fa-plus" /> Add Review
+              </button>
+            </div>
+            {this.state.reviews.map((review) => {
+              return (
+                <div
+                  key={review.id + "anime-review"}
+                  className="anime-review d-flex flex-column"
+                >
+                  <div key={review.id + "user"} className="d-flex flex-row">
+                    <img
+                      key={review.id + "img"}
+                      src={review.img}
+                      className="user-img"
+                      alt="User Image"
+                    />
+                    <div
+                      key={review.id + "div"}
+                      className="user-info d-flex flex-column"
+                    >
+                      <strong key={review.id + "strong"}> {review.name}</strong>
+                      <span key={review.id + "span"}>
+                        Rated {review.userRating} out of 10
+                      </span>
+                    </div>
+                  </div>
+                  <div
+                    key={review.id + "user-comment"}
+                    className="user-comment"
+                  >
+                    <ReactReadMoreReadLess
+                      charLimit={400}
+                      readMoreText={"Read more ▼"}
+                      readLessText={"Read less ▲"}
+                    >
+                      {review.comment}
+                    </ReactReadMoreReadLess>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </>
